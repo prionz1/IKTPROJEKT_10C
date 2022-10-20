e36ef16a7b13678617137467751d7391df9779ef
prionz1 <112930799+prionz1@users.noreply.github.com>
2022-10-20 12:23:22 +0200

    Add files via upload

---
+++ gyakorlo1/pygyakorlas.py
@@ -0,0 +1,57 @@
+# 1
+'''list = []
+
+szam1 = int(input('kérek egy számot  '))
+szam2 = int(input('kérek még egy számot  '))
+szam3 = int(input('kérek egy harmadik számot  '))
+
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def kisebb():
+    print(min(list))
+
+kisebb()
+'''
+# 2
+'''list = []
+
+szam1 = int(input('kérek egy számot  '))
+szam2 = int(input('kérek még egy számot  '))
+szam3 = int(input('kérek egy harmadik számot  '))
+
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def nagyobb():
+    print(max(list))
+
+nagyobb()
+'''
+#3
+'''x = int(input('Adj meg egy dolgozat pontszámot '))
+if x < 50:
+    print('eggyes')
+if 50 <= x < 60:
+    print('kettes')
+if 60 <= x < 70:
+    print('hármas')
+if 70 <= x < 85:
+    print('négyes')
+if x >= 85:
+    print('ötös')'''
+
+#4
+'''number = int(input('kérek egy számot'))
+if number % 3 == 0 or number % 5 == 0 :
+    print('igen osztható')
+else:
+    print('nem osztható')
+'''
+#5
+szam1 = int(input('Kérek egy számot'))
+szam2 = int(input('Kérek egy másik számot'))
+szam3 = int(input('Kérek egy harmadik számot'))
+
