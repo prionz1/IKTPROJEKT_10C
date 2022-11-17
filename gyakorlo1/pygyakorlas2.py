ebe726917deb350cca614768bad18354b9bf6702
prionz1 <112930799+prionz1@users.noreply.github.com>
2022-11-17 12:01:25 +0100

    Create pygyakorlas2.py

---
+++ gyakorlo1/pygyakorlas2.py
@@ -0,0 +1,9 @@
+number = 1
+while number <= 20 :
+    if number % 2 == 0:
+        print(number,'páros' )
+        number += 1
+    elif number % 2 == 1:
+        print(number,'páratlan' )
+        number += 1
+#---------------------------------
